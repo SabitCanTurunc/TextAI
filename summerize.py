6c6dbeef81a0a84fd9fa34dbccee2910c7834f4e
SabitCanTurunc <turuncsc@gmail.com>
2024-03-04 04:32:42 +0300

    wikipedia search & summerize

--- summerize.py
+++ summerize.py
@@ -1,65 +1,64 @@
-import bs4 as bs
-import urllib.request
+import wikipedia
 import re
 import nltk
 import heapq
 
-def get_wikipedia_summary(url, num_sentences=7):
-    # Verilen URL'den makaleyi çekme
-    scraped_data = urllib.request.urlopen(url)
-    article = scraped_data.read()
+def search_wikipedia(keyword, num_sentences=7):
+    try:
+        # Wikipedia API'sini kullanarak makaleyi çekme
+        wikipedia.set_lang("tr")  # Türkçe Wikipedia için dil ayarı
+        article = wikipedia.page(keyword)
+        article_text = article.content
 
-    # Makaleyi işleme
-    parsed_article = bs.BeautifulSoup(article, 'lxml')
-    paragraphs = parsed_article.find_all('p')
+        ##### Ön İşleme
+        # Köşeli Parantezleri ve Ekstra Boşlukları Kaldırma
+        article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
+        article_text = re.sub(r'\s+', ' ', article_text)
 
-    article_text = ""
-    for p in paragraphs:
-        article_text += p.text
+        # Özel Karakterleri ve Rakamları Kaldırma
+        formatted_article_text = re.sub('[^a-zA-ZıİğĞüÜşŞöÖçÇ\s]', ' ', article_text)
+        formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
 
-    ##### Ön İşleme
-    # Köşeli Parantezleri ve Ekstra Boşlukları Kaldırma
-    article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
-    article_text = re.sub(r'\s+', ' ', article_text)
+        ##### Metni Cümlelere Ayırma
+        sentence_list = nltk.sent_tokenize(article_text)
 
-    # Özel Karakterleri ve Rakamları Kaldırma
-    formatted_article_text = re.sub('[^a-zA-ZıİğĞüÜşŞöÖçÇ\s]', ' ', article_text)
-    formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+        ##### Kelime Frekanslarını Bulma
+        stopwords = nltk.corpus.stopwords.words('turkish')
+        word_frequencies = {}
+        for word in nltk.word_tokenize(formatted_article_text):
+            if word not in stopwords:
+                if word not in word_frequencies.keys():
+                    word_frequencies[word] = 1
+                else:
+                    word_frequencies[word] += 1
 
-    ##### Metni Cümlelere Ayırma
-    sentence_list = nltk.sent_tokenize(article_text)
+        maximum_frequncy = max(word_frequencies.values())
+        for word in word_frequencies.keys():
+            word_frequencies[word] = (word_frequencies[word] / maximum_frequncy)
 
-    ##### Kelime Frekanslarını Bulma
-    stopwords = nltk.corpus.stopwords.words('turkish')
-    word_frequencies = {}
-    for word in nltk.word_tokenize(formatted_article_text):
-        if word not in stopwords:
-            if word not in word_frequencies.keys():
-                word_frequencies[word] = 1
-            else:
-                word_frequencies[word] += 1
+        ##### Cümlenin Skorunu Hesaplama
+        sentence_scores = {}
+        for sent in sentence_list:
+            for word in nltk.word_tokenize(sent.lower()):
+                if word in word_frequencies.keys():
+                    if len(sent.split(' ')) < 30:
+                        if sent not in sentence_scores.keys():
+                            sentence_scores[sent] = word_frequencies[word]
+                        else:
+                            sentence_scores[sent] += word_frequencies[word]
 
-    maximum_frequncy = max(word_frequencies.values())
-    for word in word_frequencies.keys():
-        word_frequencies[word] = (word_frequencies[word] / maximum_frequncy)
+        ###### Anahtar Kelime İçeren Cümlelerden Özet Oluşturma
+        summary_sentences = heapq.nlargest(num_sentences, sentence_scores, key=sentence_scores.get)
+        summary = ' '.join(summary_sentences)
+        return summary
 
-    ##### Cümlenin Skorunu Hesaplama
-    sentence_scores = {}
-    for sent in sentence_list:
-        for word in nltk.word_tokenize(sent.lower()):
-            if word in word_frequencies.keys():
-                if len(sent.split(' ')) < 30:
-                    if sent not in sentence_scores.keys():
-                        sentence_scores[sent] = word_frequencies[word]
-                    else:
-                        sentence_scores[sent] += word_frequencies[word]
+    except wikipedia.exceptions.PageError:
+        return "Arama sonuçları bulunamadı."
 
-    ###### Özet Oluşturma
-    summary_sentences = heapq.nlargest(num_sentences, sentence_scores, key=sentence_scores.get)
-    summary = ' '.join(summary_sentences)
-    return summary
+    except wikipedia.exceptions.DisambiguationError:
+        return "Arama sonuçları bulunamadı."
 
-# Test 
-url = 'https://tr.wikipedia.org/wiki/telefon'
-summary = get_wikipedia_summary(url)
+# Anahtar kelimeyi kullanıcıdan al
+keyword = input("Anahtar kelimeyi girin: ")
+summary = search_wikipedia(keyword)
 print(summary)
