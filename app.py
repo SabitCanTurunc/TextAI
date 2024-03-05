e23c46c0900bb6172585eef68f4fc2858f8cdfe2
SabitCanTurunc <turuncsc@gmail.com>
2024-03-05 18:53:45 +0300

    app test

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from dataPreprocessing import *
-import logging
-import string
+
 from collections import Counter
 from zemberek import TurkishMorphology, TurkishTokenizer,TurkishSentenceNormalizer
 from nltk.corpus import stopwords as nltk_stopwords
+from summerize import *
+
 
 # SENTENCE NORMALIZATION
 # Cümle normalizasyonu için nesne oluşturuluyor
@@ -11,7 +12,13 @@ morphology = TurkishMorphology.create_with_defaults()
 normalizer = TurkishSentenceNormalizer(morphology)
 
 
-metin = input("Lütfen metni girin: ")
+metin="Yapay zeka, bilgisayar bilimlerinde ve yapay zeka araştırmalarında kullanılan bir terimdir. Yapay zeka, bilgisayar sistemlerine insan benzeri zeka yetenekleri kazandırmayı amaçlayan bir bilim dalıdır. Bu yetenekler arasında öğrenme, problem çözme, algılama, dil anlama ve daha pek çok şey bulunur. Yapay zeka, algoritmaların ve veri setlerinin kullanımıyla bilgisayarların karmaşık görevleri gerçekleştirebilmesini sağlar.Yapay zeka, pek çok farklı alanda kullanılmaktadır. Örneğin, otomasyon, robotik, dil işleme, görüntü işleme, oyunlar, finansal analiz, tıp alanında teşhis ve tedavi planlama gibi alanlarda yaygın olarak kullanılmaktadır.Ancak yapay zeka teknolojisinin etik ve sosyal etkileri de bulunmaktadır. Örneğin, iş gücü piyasalarında değişikliklere neden olabilir, kişisel mahremiyetin ihlal edilmesine yol açabilir veya ayrımcılığı artırabilir. Bu nedenle, yapay zeka teknolojisinin geliştirilmesi ve kullanımıyla ilgili olarak etik kurallar ve yasal düzenlemeler oldukça önemlidir."
+#     
+# Metni özetle
+summary = summarize_article(metin)
+
+# Özetlenmiş metni yazdır
+print("özet: ",summary)
 
 # Normalizasyon 
 normalized_metin = normalizer.normalize(metin)
@@ -28,10 +35,12 @@ print("\nYardımcı Konular ve Tekrar Sayıları:")
 for kelime, sayi in yardimci_konular:
     print(f"{kelime}: {sayi}")
 
-# # Ana konunun köklerini bulma ve yazdırma
-# ana_konu_kokler_zemberek = kelimeleri_koklere_ayir_zemberek([ana_konu_kelimesi])
-# print("\nAna Konunun Kökleri (Zemberek):", ana_konu_kokler_zemberek)
-
-# # Yardımcı konuların köklerini bulma ve yazdırma
-# yardimci_konular_kokler_zemberek = kelimeleri_koklere_ayir_zemberek([kelime for kelime, _ in yardimci_konular])
-# print("Yardımcı Konuların Kökleri (Zemberek):", yardimci_konular_kokler_zemberek)
+# Anahtar kelimeyi kullanıcıdan al
+keyword = ana_konu_kelimesi
+print("\nBenzer konular: ")
+article_text = search_wikipedia(keyword[0:])
+if article_text != "Arama sonuçları bulunamadı.":
+    summary = summarize_article(article_text)
+    print(summary)
+else:
+    print(article_text)
